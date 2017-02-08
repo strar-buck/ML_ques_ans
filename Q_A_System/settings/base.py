4e1639a549279e12f8cb84019565c516951da4ef
djangoconfire <djangopycon@gmail.com>
2017-02-08 17:04:21 +0530

    modify base.py

--- Q_A_System/settings/base.py
+++ Q_A_System/settings/base.py
@@ -121,8 +121,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_URL='/media/'
-
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static','static_root')
 
@@ -131,4 +129,3 @@ STATICFILES_DIRS = [
    os.path.join(BASE_DIR,'static'),
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- Q_A_System/settings/local.py
+++ Q_A_System/settings/local.py
@@ -121,12 +121,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_URL='/media/'
-
-STATIC_DIR=BASE_DIR + '/static/'
 
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR,'static'),
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static','static_root')
